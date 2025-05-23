effb9dcfc4dc6e135b494b955fc87e017de8d3dd
RhichelleStrauss <251169@virtualwindow.co.za>
2025-05-23 14:51:33 +0200

    working counters on flights!!
    counters on flights page fully functional

--- js/script.js
+++ js/script.js
@@ -1,30 +1,97 @@
 //button counters on flights page 
+let plus = document.querySelector("marsPlus"); //making mars the default plus, and reference back to html 
+let minus = document.querySelector("marsMinus"); //making mars the default minus, and reference back to html
+let num = document.querySelector("marsNum"); //making mars the default num, and reference back to html
 
+let marsCounter = 0;
+let neptuneCounter = 0;
+let jupiterCounter = 0;
+let saturnCounter = 0;
+let uranusCounter = 0;
+let moonCounter = 0;
+//num = 0 default on all counters 
 
-const plus = document.querySelector (".plus");
-minus = document.querySelector (".minus"),
-num = document.querySelector (".num");
+//event listeners to search for correct planet 
+function SetSelector(planetString){
+  plus = document.querySelector (`.${planetString}Plus`);
+  minus = document.querySelector (`.${planetString}Minus`);
+  num = document.querySelector (`.${planetString}Num`);
 
+}
 
-let a = 1;
-
-plus.addEventListener("click", ()=>{
-
-    a++;
-    console.log(a);
-    num.innerText = a;
-
-});
-
-
-minus.addEventListener("click", ()=>{
-
-    if (a > 1){
-        a--;
-        num.innerText = a;
+function Plus(planetString) {
+  SetSelector(planetString);
+    if (planetString === "mars") {
+      marsCounter++;
+      console.log(marsCounter);
+      num.innerHTML = marsCounter;
+    } else if (planetString === "neptune") {
+      neptuneCounter++;
+      console.log(neptuneCounter);
+      num.innerHTML = neptuneCounter;
     }
+    else if (planetString === "jupiter") {
+      jupiterCounter++;
+      console.log(jupiterCounter);
+      num.innerHTML = jupiterCounter;
+    }
+    else if (planetString === "saturn") {
+      saturnCounter++;
+      console.log(saturnCounter);
+      num.innerHTML = saturnCounter;
+    }
+    else if (planetString === "uranus") {
+      uranusCounter++;
+      console.log(uranusCounter);
+      num.innerHTML = uranusCounter;
+    }
+    else if (planetString === "moon") {
+      moonCounter++;
+      console.log(moonCounter);
+      num.innerHTML = moonCounter;
+    }
+}//if runs through all code to check which planet is being counted and ensures they have seperate values 
 
-});
+
+
+function Minus(planetString) {
+  SetSelector(planetString);
+    if (planetString === "mars" && marsCounter > 0) { //ensures minus doesnt go beyond 0
+      marsCounter--;
+      console.log(marsCounter);
+      num.innerHTML = marsCounter;
+    } else if (planetString === "neptune" && neptuneCounter > 0) {
+      neptuneCounter--;
+      console.log(neptuneCounter);
+      num.innerHTML = neptuneCounter;
+    }
+    else if (planetString === "jupiter" && jupiterCounter > 0) {
+      jupiterCounter--;
+      console.log(jupiterCounter);
+      num.innerHTML = jupiterCounter;
+    }
+    else if (planetString === "saturn" && saturnCounter > 0) {
+      saturnCounter--;
+      console.log(saturnCounter);
+      num.innerHTML = saturnCounter;
+    }
+    else if (planetString === "uranus" && uranusCounter > 0) {
+      uranusCounter--;
+      console.log(uranusCounter);
+      num.innerHTML = uranusCounter;
+    }
+    else if (planetString === "moon" && moonCounter > 0) {
+      moonCounter--;
+      console.log(moonCounter);
+      num.innerHTML = moonCounter;
+    }
+}
+
+
+
+
+
+
 
 //thank you popup on form 
 
